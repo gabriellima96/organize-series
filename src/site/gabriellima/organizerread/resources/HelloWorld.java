de1b315e8808ef25b98ae345ece871de3ccd52ad
Gabriel Lima <gabriellima.silva96@gmail.com>
2018-11-24 18:40:23 -0300

    Adicionando o projeto

---
+++ src/site/gabriellima/organizerread/resources/HelloWorld.java
@@ -0,0 +1,17 @@
+package site.gabriellima.organizerread.resources;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+@Path("/")
+public class HelloWorld {
+
+	@GET
+	@Produces(value = MediaType.APPLICATION_JSON)
+	public Response helloWorld() {
+		return Response.ok().entity("Hello World").build();
+	}
+}
